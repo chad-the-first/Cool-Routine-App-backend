da7ea91a77188de50c49c139d606fa6498fefdd6
Chad <worktannoury@gmail.com>
2023-07-28 12:18:03 +0300

    Add a Schema for db and handle errors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
 import "dotenv/config";
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import RoutineModel from "./models/routine"
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Habibi Come to Dubai');
+app.get('/', async (req, res, next) => {
+    try {
+        throw Error('Mahboul shi?');
+        const routines = await RoutineModel.find().exec();
+        res.status(200).json(routines);
+    } catch (error) {
+        next(error);
+    }
 } );
+
+app.use((req, res, next) => {
+    next(Error("Endpoint not found"))
+})
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occurred";
+    if (error instanceof Error) errorMessage = error.message;
+    res.status(500).json({ error: errorMessage });
+})
 
 export default app;
---
+++ src/models/routine.ts
@@ -0,0 +1,11 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+
+const routineSchema = new Schema({
+    title: { type: String, required: true },
+    text: { type: String },
+}, { timestamps: true});
+
+type Routine = InferSchemaType<typeof routineSchema>;
+
+export default model<Routine>("Routine", routineSchema);
