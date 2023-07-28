809a9fc4c8d18414b2fc294096215aa71be7e33e
Chad <worktannoury@gmail.com>
2023-07-28 15:43:56 +0300

    Upgrade handling errors

--- src/controllers/routines.ts
+++ src/controllers/routines.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import RoutineModel from "../models/routine"
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 
 export const getRoutines: RequestHandler = async (req, res, next) => {
@@ -15,18 +17,36 @@ export const getRoutine: RequestHandler = async (req, res, next) => {
     const id = req.params.routineId;
 
     try {
-        const routine = await RoutineModel.findById(id).exec();
+        if (!mongoose.isValidObjectId(id)) {
+            throw createHttpError(400, "invalide Routine Id");
+        }
+
+        const routine = await RoutineModel.findById(id).exec(); 
+
+        if (!routine) {
+            throw createHttpError(404, 'routine not found');
+        }
+
         res.status(200).json(routine);
     } catch (error) {
         next(error);
     }
 }
 
-export const createRoutine: RequestHandler = async (req, res, next) => {
+interface CreateRoutineBody {
+    title?: string,
+    text?: string,
+}
+
+export const createRoutine: RequestHandler<unknown, unknown, CreateRoutineBody, unknown> = async (req, res, next) => {
     const title = req.body.title;
     const text = req.body.text;
 
     try {
+        if (!title) {
+            throw createHttpError(400, "Routine must have a title.")
+        }
+
         const newRoutine = await RoutineModel.create({
             title: title,
             text: text,
