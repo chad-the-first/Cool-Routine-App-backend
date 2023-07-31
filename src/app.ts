eba67f02c92fd90b6ab32c7993a8ad3b044b517b
Chad <worktannoury@gmail.com>
2023-07-31 13:39:02 +0300

    Impliment authentication

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import routineRoutes from "./routes/routines"
+import userRoutes from "./routes/users"
 import morgan from "morgan"; 
 import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
+import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
 
 const app = express();
 
@@ -10,7 +14,21 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
-app.use("/api/routines", routineRoutes)
+app.use(session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+        mongoUrl: env.MONGO_CONNECTION_STRING
+    }),
+}))
+
+app.use("/api/users", userRoutes);
+app.use("/api/routines", routineRoutes);
 
 app.use((req, res, next) => {
     next(createHttpError(404, "Endpoint not found"));
