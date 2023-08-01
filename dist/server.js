22590bf474aea686315c8fd8251af8c3c72fec1a
Chad <worktannoury@gmail.com>
2023-08-01 19:47:23 +0300

    change port

--- dist/server.js
+++ dist/server.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const validateEnv_1 = __importDefault(require("./util/validateEnv"));
 const mongoose_1 = __importDefault(require("mongoose"));
-const port = validateEnv_1.default.PORT;
+const port = process.env.PORT;
 mongoose_1.default.connect(validateEnv_1.default.MONGO_CONNECTION_STRING)
     .then(() => {
     console.log("Mongoose connected");
